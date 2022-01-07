c82c11c9676f6bbee8bd16aa1f473b6c0f01b8e0
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-07 01:55:57 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px  # (version 4.7.0 or higher)
+#import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
 
