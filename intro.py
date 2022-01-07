b193465136bcfd2175a1a95cfffffd4b9e566e27
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-07 02:14:42 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -94,5 +94,5 @@ def update_graph(option_slctd):
 
 
 # ------------------------------------------------------------------------------
-#if __name__ == '__main__':
- #   app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
